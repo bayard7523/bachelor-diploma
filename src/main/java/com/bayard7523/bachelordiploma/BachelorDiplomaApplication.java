b18dd0d14f86c6a2501e0822bd0c481b8663016b
bayard7523 <zheniayakymchuck@gmail.com>
2022-05-22 10:15:54 +0300

    - initial commit

---
+++ src/main/java/com/bayard7523/bachelordiploma/BachelorDiplomaApplication.java
@@ -0,0 +1,13 @@
+package com.bayard7523.bachelordiploma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BachelorDiplomaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BachelorDiplomaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bayard7523/bachelordiploma/BachelorDiplomaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bayard7523.bachelordiploma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BachelorDiplomaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
